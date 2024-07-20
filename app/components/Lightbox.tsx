eba5617d24a0e58742257855822c85c7b888b4b5
integral_digi <ojaduagreat@gmail.com>
2024-07-20 08:41:14 +0100

    fixed reel size

--- app/components/Lightbox.tsx
+++ app/components/Lightbox.tsx
@@ -32,12 +32,12 @@ const Lightbox = ({ media, isOpen, setIsOpen }: LightboxProps) => {
   return (
     <div className="w-full lg:justify-center lg:w-screen h-screen lg:left-0 bg-zinc-800">
       {isOpen && (
-        <div className="fixed top-0 w-[50%] h-fit flex items-center justify-center z-50 aspect-auto m-auto lg:w-full lg:h-full lg:block">
-          <div className="w-3/4 flex justify-center">
+        <div className="fixed top-0 w-1/2 h-fit flex items-center justify-center z-50 aspect-auto m-auto lg:w-full lg:h-full lg:block">
+          <div className="">
             <Progress progress={0} />
           </div>
-          <div className="lightbox-overlay fixed top-0 left-0 w-full h-full bg-black opacity-80"></div>
-          <div className="lightbox rounded-lg h-screen w-[500px] relative">
+          <div className="lightbox-overlay fixed top-0 left-0 w-full h-full bg-black opacity-80" />
+          <div className="lightbox rounded-lg h-screen w-full relative">
             <div className="absolute bottom-12 mx-auto z-50 justify-center w-full px-12">
                 <AddReelComment />
             </div>
